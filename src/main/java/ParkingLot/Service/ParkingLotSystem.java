8a1fb842688c3d6f27264f617bff524b2528d22e
vegirevathi <vegirevathi@gmail.com>
2020-07-17 22:52:50 +0530

    ADDED: Method to check parking status

--- src/main/java/ParkingLot/Service/ParkingLotSystem.java
+++ src/main/java/ParkingLot/Service/ParkingLotSystem.java
@@ -1,4 +1,14 @@
 package ParkingLot.Service;
 
 public class ParkingLotSystem {
+    public int numberOfCarsParked;
+
+    public ParkingLotSystem(int numberOfCarsParked) {
+        this.numberOfCarsParked = numberOfCarsParked;
+    }
+
+    public boolean parkingStatus() {
+        if (numberOfCarsParked < 100) return true;
+        return false;
+    }
 }
